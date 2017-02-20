44bdef9ed21b3ee4e1d773f07ced080dbee78dbd
Marc Brinkmann <git@marcbrinkmann.de>
2017-02-20 23:41:47 +0100

    Working gitlab instantiation.

--- git_hulahoop/cli.py
+++ git_hulahoop/cli.py
@@ -11,28 +11,19 @@ from .git import GitRepo
               default='.',
               type=click.Path(),
               help='Path to the repository to work on (default: .')
-@click.option(
-    '-D',
-    '--default-remote',
-    default='origin',
-    help='Remote to check for remote host if none given (default: origin')
 @click.option('-R',
               '--remote',
-              default=None,
-              help='Remote to check for remote host')
+              default='origin',
+              help='Remote to check for remote host (default: origin)')
 @click.option('-T',
               '--remote-type',
               type=click.Choice(['auto', 'gitlab'], ),
               default='auto',
               help='API type to use')
 @click.pass_context
-def cli(ctx, repo, default_remote, remote, remote_type):
-    cfg = {'repo_path': repo,
-           'default_remote': default_remote,
-           'remote': remote, }
-    ctx.obj = {'cfg': cfg,
-               'repo': GitRepo(repo, remote or default_remote, None,
-                               remote_type), }
+def cli(ctx, repo, remote, remote_type):
+    cfg = {'repo_path': repo, 'remote': remote, }
+    ctx.obj = {'cfg': cfg, 'repo': GitRepo(repo, remote, None, remote_type), }
 
 
 @cli.group('issue', help='Handle issues')
@@ -44,9 +35,8 @@ def issue():
 @click.pass_obj
 def list_issues(obj):
     repo = obj['repo']
-    remote = repo.get_remote()
     click.echo('Host: {}'.format(repo.host))
-    click.echo('Remote: {}'.format(remote))
+    click.echo('Remote: {}'.format(repo.remote))
     pass
 
 
